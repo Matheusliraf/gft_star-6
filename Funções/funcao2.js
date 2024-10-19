b457b3ee2fd69828836da051ad62aa69cdecaaaa
Matheusliraf <matheusliraf.23@gmail.com>
2024-10-18 21:51:14 -0300

    atualização

---
+++ Funções/funcao2.js
@@ -0,0 +1,22 @@
+main()
+
+function main(){
+    getData()
+    checkvalues()
+    sendToDatabase()
+}
+
+function getData(){
+    console.log("Pegando os dados do usuário")
+
+}
+
+function checkvalues(){
+    console.log("Validando os dados do usuario")
+
+}
+
+function sendToDatabase(){
+    console.log("Cadastrando dados")
+
+}
