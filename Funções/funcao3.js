471a4113869ea58f7c0339f48227669f8efa1aa5
Matheusliraf <matheusliraf.23@gmail.com>
2024-10-20 10:32:57 -0300

    autalização

---
+++ Funções/funcao3.js
@@ -0,0 +1,5 @@
+createStringConnection("db_products", "felipe", "9876")
+
+function createStringConnection(databaseNeme, user, pass){
+    console.log(`connect:DDCONNECT:usert=${user};pass=${pass},initial_database=${databaseNeme}}`)
+}
---
+++ Funções/funcoesComParametros.js
@@ -0,0 +1,10 @@
+torrar("pão de forma", 19.99,"matheus")
+
+
+
+function torrar(pao, valor ,nome = "Cliente"){
+    console.log("Torrada feita com " + pao)
+    console.log("ela é um pedido do " + nome)
+    console.log("E o valor de R$"+valor)
+}
+
